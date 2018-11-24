a8029407babd863df62dc3f20ee7dd83353ce23c
Woody Lucas <wlucas0311@gmail.com>
2018-11-24 06:57:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,7 +16,8 @@ def loop_iterator(number_of_times)
   loop do 
     puts phrase 
     counter += 1
-    break if counter >= number_of_times 
+     if counter >= number_of_times 
+       break 
     end 
     
   
